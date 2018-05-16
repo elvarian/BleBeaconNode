b8f139cb2e86a51523cf7274b183a3a8e136ce8a
Liv Heusala <liv@kapsi.fi>
2018-05-16 16:27:24 +0300

    trying to add more output

--- bleread.py
+++ bleread.py
@@ -198,7 +198,7 @@ def main():
 
     packetsSend = 0
     
-    lastLog = date.min
+    lastLog = datetime.min
 
     while gotOK < 50:
       reply = reader.stdout.readline()
@@ -233,7 +233,7 @@ def main():
      #print 'line now: ' + printline
      #print 'line header: ' + header
 
-      if datetime.today() - timedelta(minutes=10) > lastLog:
+      if datetime.now() - timedelta(minutes=10) > lastLog:
           print packetsSend + " packets sent"
           packetsSend = 0
           lastLog = datetime.now()
